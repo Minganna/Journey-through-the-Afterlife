1985b0abcf4e90650e1737d03cefca8de213f38b
Marco Minganna <s4929892@bournemouth.ac.uk>
2019-04-02 15:37:36 +0100

    Attempt Bug Fix 7

---
+++ JourneyThroughTheAfterlife/Assets/Script/controller/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform Target;
+    public float SmoothSpeed=1.125f;
+    public Vector3 Offset;
+
+    void FixedUpdate ()
+    {
+        Vector3 DesiredPosition = Target.position + Offset;
+        Vector3 SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed*Time.deltaTime);
+        transform.position = SmoothPosition;
+
+        transform.LookAt(Target);
+    }
+}
