2f6c9207f1977e7329cce60266d79fdff9a756c2
Marco Minganna <s4929892@bournemouth.ac.uk>
2019-04-05 14:39:13 +0100

    Glowing Shader Added

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/SaveAndPause/ZoomMinimap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomMinimap : MonoBehaviour {
+
+
+	public Camera minimapCamera;
+	float zoom;
+
+	void Start()
+	{
+		zoom=minimapCamera.orthographicSize;
+	}
+
+	void Update()
+	{
+		minimapCamera.orthographicSize = zoom;
+	}
+	public void MinusZoom()
+	{
+
+	
+			zoom += 10;	
+
+
+	}
+	public void PlusZoom()
+	{
+	if (zoom > 9.5) {
+		zoom -= 10;	
+		}
+	}
+}
