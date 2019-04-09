9f66772e7afc861cd76a67dd8fd89f64f963d652
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2019-04-09 13:42:15 +0100

    a

---
+++ afterlife/Assets/CustomFolders/Script/Player/Daniil_TestPushRigid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daniil_TestPushRigid : MonoBehaviour {
+
+	public float pushPower = 2.0f;
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		Rigidbody rB = hit.collider.attachedRigidbody;
+
+		if (rB == null) {
+			return;
+		}
+
+
+		Vector3 pushDir = new Vector3 (hit.moveDirection.x, 0, hit.moveDirection.z);
+
+		rB.velocity = pushDir * pushPower;
+		Debug.Log ("Hit");
+	}
+}
