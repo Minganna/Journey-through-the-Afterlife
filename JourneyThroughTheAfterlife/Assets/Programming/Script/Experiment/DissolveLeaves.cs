50278f59753acd76573c9cd266b5ad71c76e5502
Marco Minganna <s4929892@bournemouth.ac.uk>
2019-04-17 13:18:58 +0100

    leaves dissolves with proximity

--- JourneyThroughTheAfterlife/Assets/Programming/Script/Experiment/DissolveLeaves.cs
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Experiment/DissolveLeaves.cs
@@ -6,17 +6,34 @@ public class DissolveLeaves : MonoBehaviour {
 
     private Renderer MyRendered;
     float Threshold;
-    bool dissolvestarting = false;
+   public bool dissolvestarting = false;
+    public int numbertree = 0;
+   public  LeavesController wallcounter1;
+  public  LeavesController wallcounter2;
 
     // Use this for initialization
     void Start () {
         MyRendered = gameObject.GetComponent<Renderer>();
         MyRendered.material.shader= Shader.Find("Custom/Dissolve");
-        StartCoroutine(StartDissolving());
+        wallcounter1 = GameObject.FindGameObjectWithTag("wallone").GetComponent<LeavesController>();
+        wallcounter2 = GameObject.FindGameObjectWithTag("walltwo").GetComponent<LeavesController>();
+        if (numbertree == 0)
+        {
+            StartCoroutine(StartDissolving());
+        }
+      
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (wallcounter1.activate1 == true && numbertree == 1)
+        {
+            dissolvestarting = true;
+        }
+        if (wallcounter2.activate2 == true && numbertree == 2)
+        {
+            dissolvestarting = true;
+        }
         if (dissolvestarting)
         {
             Threshold += 0.001f;
---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Experiment/LeavesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeavesController : MonoBehaviour {
+
+    public int numbwall=0;
+    public bool activate1;
+    public bool activate2;
+	
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (numbwall==0)
+        {
+            activate1 = true;
+        }
+        if (numbwall == 1)
+        {
+            activate2 = true;
+        }
+    }
+
+}
