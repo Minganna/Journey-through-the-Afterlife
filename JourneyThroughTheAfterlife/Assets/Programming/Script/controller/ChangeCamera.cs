79b613fa19099fab57008df1e8976c43834d1c15
Marco Minganna <s4929892@bournemouth.ac.uk>
2019-04-29 12:33:23 +0100

    Camera Updated

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/ChangeCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+
+	public CameraFollow changeoffset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter()
+	{
+		changeoffset.Offset = new Vector3 (-10, 4, 0);
+	}
+	void OnTriggerExit()
+	{
+		changeoffset.Offset = new Vector3 (0, 2, -10);
+	}
+}
---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/JumpOnCanoe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpOnCanoe : MonoBehaviour {
+	public GameObject Player;
+	public Rigidbody Canoe;
+	private bool MoveCanoe;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (MoveCanoe) {
+			Canoe.AddForce (transform.right);
+		}
+	}
+
+	void OnTriggerEnter()
+	{
+		Player.SetActive (false);
+		MoveCanoe = true;
+
+	}
+}
