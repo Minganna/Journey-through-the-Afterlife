5e841fe62658c6e4888f9284e8f2d9d69908c3b8
Jesse Nunes <s5081040@bournemouth.ac.uk>
2019-05-01 16:20:59 +0100

    New car added

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Misc/WheelRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotation : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		gameObject.transform.RotateAround(gameObject.transform.position,Vector3.right,500*Time.deltaTime);
+		
+	}
+}
