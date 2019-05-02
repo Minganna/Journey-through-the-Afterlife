92e32ea58bd4e9c97ef1691d7e70f2ce163794fd
ropi336 <greg33615@gmail.com>
2019-05-02 21:59:05 +0100

    Added sounds and music to the start menu, added rain sfx to the starting level

--- JourneyThroughTheAfterlife/Assets/Programming/Script/controller/greg/mainmenu_buttons.cs
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/greg/mainmenu_buttons.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainmenu_buttons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool loadingScene = false;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (loadingScene)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
 }
