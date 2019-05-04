8089da5446e83831dcd76c6259998b0203a0b907
ropi336 <greg33615@gmail.com>
2019-05-04 19:02:20 +0100

    Added scrolling floor. But.. it doesn't really stop. Ever.

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/greg/mainmenu_ground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu_ground : MonoBehaviour {
+
+    public GameObject[] GroundComponents = new GameObject[3];
+    const float GroundLength = 118f;
+
+    public float GroundSpeed = 14f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject Ground in GroundComponents)
+        {
+            Vector3 newGroundPos = Ground.transform.position;
+            newGroundPos.z -= GroundSpeed * Time.deltaTime;
+            if (newGroundPos.z < -GroundLength / 2)
+            {
+                newGroundPos.z += GroundLength;
+            }
+            Ground.transform.position = newGroundPos;
+
+        }
+    }
+}
+
