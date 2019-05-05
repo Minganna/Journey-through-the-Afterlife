fbd8077cf6e9922dbdbbdd768cba52e454d0584d
Megan <mgibsonmandalore@gmail.com>
2019-05-05 17:03:27 +0100

    Boss Battle

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Boss Battle/SnakeMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+	public Animator anim;
+
+	void start (){
+		GetComponent<Animator> ();
+	}
+
+	private void OnTriggerEnter ()
+	{
+
+	}
+
+}
