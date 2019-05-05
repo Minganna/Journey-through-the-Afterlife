f709225b9dfc48b3843a7af73038aec006a38665
Marco Minganna <s4929892@bournemouth.ac.uk>
2019-05-05 18:47:10 +0100

    Cerberus added and started

--- JourneyThroughTheAfterlife/Assets/Programming/Script/NPC/JudgeSight.cs
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/NPC/JudgeSight.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class JudgeSight : MonoBehaviour {
 
     public Dissolve dissolve;
+    public DialogueTrigger diatri;
+    public CameraFollow cam;
+    bool spot=false;
+    public GameObject[] Player;
+   
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player"&&dissolve.CanBeSpotted==true)
 		{
-			Debug.Log ("Spotted");
+            if (spot == false)
+            {
+                diatri.TriggerDialogue();
+                StartCoroutine(CheckPoint());
+            }
 		}
 
 	}
+
+    IEnumerator CheckPoint()
+    {
+        yield return new WaitForSeconds(2f);
+        diatri.NextSentence();
+        Player[0].transform.position = Player[1].transform.position;
+        cam.Offset = new Vector3(-10, 4, 0);
+
+    }
 }
--- JourneyThroughTheAfterlife/Assets/Programming/Script/controller/ChangeCamera3.cs
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/ChangeCamera3.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class ChangeCamera3 : MonoBehaviour {
     public CameraFollow changeoffset;
-    public MeshCollider cave;
+
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             changeoffset.Offset = new Vector3(0,2,10);
-            cave.convex = true;
 
         }
     }
