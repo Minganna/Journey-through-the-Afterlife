97af154cf00ee47c8272c1dc66e8424e71b29998
Megan <mgibsonmandalore@gmail.com>
2019-05-05 13:37:18 +0100

    Asphodel

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Misc/MemoryItem.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryItem : MonoBehaviour {
+
+    public bool isAnimated = false;
+
+    public bool isRotating = false;
+    public bool isFloating = false;
+
+    public Vector3 rotationAngle;
+    public float rotationSpeed;
+
+    public float floatSpeed;
+    private bool goingUp = true;
+    public float floatRate;
+    private float floatTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        
+        if(isAnimated)
+        {
+            if(isRotating)
+            {
+                transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+            }
+
+            if(isFloating)
+            {
+                floatTimer += Time.deltaTime;
+                Vector3 moveDir = new Vector3(0.0f, 0.0f, floatSpeed);
+                transform.Translate(moveDir);
+
+                if (goingUp && floatTimer >= floatRate)
+                {
+                    goingUp = false;
+                    floatTimer = 0;
+                    floatSpeed = -floatSpeed;
+                }
+
+                else if(!goingUp && floatTimer >= floatRate)
+                {
+                    goingUp = true;
+                    floatTimer = 0;
+                    floatSpeed = +floatSpeed;
+                }
+            }
+          
+	}
+}
+
+}
