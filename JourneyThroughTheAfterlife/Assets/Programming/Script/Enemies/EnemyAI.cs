5048b4d5f22369b123951521f99b7e735e669f63
Megan Gibson (s5062812) <s5062812@bournemouth.ac.uk>
2019-05-06 14:28:38 +0100

    EnemyAI

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Enemies/EnemyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	public GameObject enemy;
+	Animator anim;
+	public Transform player;
+	public float moveSpeed = 4f;
+	private float minDist = 1f;
+
+	// Use this for initialization
+	void Start () {
+		anim = enemy.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, player.position) >= minDist) {
+			transform.LookAt (player);
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+			enemy.GetComponent<Animator> ().SetBool ("Aggro", true);
+		} else {
+			enemy.GetComponent<Animator> ().SetBool ("Aggro", false);
+		}
+	}
+
+	void OnTriggerEnter(){
+		enemy.GetComponent<Animator> ().SetBool ("Attack", true);
+	}
+
+	void OnTriggerExit(){
+		enemy.GetComponent<Animator> ().SetBool ("Attack", false);
+	}
+}
---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Enemies/EnemyNoMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNoMove : MonoBehaviour {
+	public GameObject enemy;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = enemy.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		enemy.GetComponent<Animator> ().SetBool ("Attack", true);
+	}
+
+	void OnTriggerExit(){
+		enemy.GetComponent<Animator> ().SetBool ("Attack", false);
+	}
+}
