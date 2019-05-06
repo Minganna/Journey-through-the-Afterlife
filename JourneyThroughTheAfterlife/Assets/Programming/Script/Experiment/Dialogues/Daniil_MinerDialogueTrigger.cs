691fcf5d113f74324ef79b25441fe6398b353807
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2019-05-07 00:21:42 +0100

    Finished quest in a hall with a tree

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Experiment/Dialogues/Daniil_MinerDialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daniil_MinerDialogueTrigger : MonoBehaviour {
+	public DialogueTrigger StartDialogue;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			StartDialogue.TriggerDialogue ();
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		StartDialogue.NextSentence ();
+	}
+}
---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Player/Danill/MinerQuestTeddy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerQuestTeddy : MonoBehaviour {
+	GameObject QuestItem;
+	bool QuestDone = false;
+	public Animator _animator;
+
+	// Update is called once per frame
+	void Update () {
+		if (QuestDone == true) {
+			Destroy (GameObject.FindGameObjectWithTag("DeletePadlock"));
+			_animator.SetBool ("Open", true);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "Teddy_Textured") {
+			QuestDone = true;
+			Destroy (other.gameObject);
+
+		}
+	}
+}
