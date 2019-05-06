81f89628296123e448fc8767d3f5acfa3bc06fdf
Megan Gibson (s5062812) <s5062812@bournemouth.ac.uk>
2019-05-06 16:04:38 +0100

    Made Script Greg Friendly

--- JourneyThroughTheAfterlife/Assets/Programming/Script/Misc/MemoryItem.cs
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Misc/MemoryItem.cs
@@ -6,15 +6,10 @@ public class MemoryItem : MonoBehaviour {
     public bool isAnimated = false;
 
     public bool isRotating = false;
-    public bool isFloating = false;
 
     public Vector3 rotationAngle;
     public float rotationSpeed;
 
-    public float floatSpeed;
-    private bool goingUp = true;
-    public float floatRate;
-    private float floatTimer;
 
 	// Use this for initialization
 	void Start () {
@@ -33,26 +28,7 @@ public class MemoryItem : MonoBehaviour {
                 transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
             }
 
-            if(isFloating)
-            {
-                floatTimer += Time.deltaTime;
-                Vector3 moveDir = new Vector3(0.0f, 0.0f, floatSpeed);
-                transform.Translate(moveDir);
-
-                if (goingUp && floatTimer >= floatRate)
-                {
-                    goingUp = false;
-                    floatTimer = 0;
-                    floatSpeed = -floatSpeed;
-                }
-
-                else if(!goingUp && floatTimer >= floatRate)
-                {
-                    goingUp = true;
-                    floatTimer = 0;
-                    floatSpeed = +floatSpeed;
-                }
-            }
+           
           
 	}
 }
