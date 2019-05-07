a5179dd8789a1ae836fb06d4e19dae8a4291d091
ropi336 <greg33615@gmail.com>
2019-05-07 10:22:04 +0100

    instructions added

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/SaveAndPause/MainMenu/Instructions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour {
+
+    public GameObject SettingsMenu;
+    public GameObject Text;
+    public GameObject Button;
+
+    public void Instruction()
+    {
+        Text.SetActive(true);
+        SettingsMenu.SetActive(false);
+        Button.SetActive(true);
+    }
+
+    public void ComeBack()
+    {
+        Text.SetActive(false);
+        SettingsMenu.SetActive(true);
+        Button.SetActive(false);
+    }
+}
