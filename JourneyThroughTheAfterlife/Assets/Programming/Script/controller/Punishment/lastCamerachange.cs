e6497769635724b365fb2c4c6b981e7cb8ea5f09
ODDINProgrammer <daniilosit2@gmail.com>
2019-05-07 10:56:11 +0100

    alpha build final

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/controller/Punishment/lastCamerachange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lastCamerachange : MonoBehaviour {
+
+    public CameraFollow cam;
+    public int numb = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && numb == 0)
+        {
+            cam.Offset = new Vector3(-10, 2, 0);
+        }
+        if (other.tag == "Player" && numb == 1)
+        {
+            cam.Offset = new Vector3(20, 2, 0);
+        }
+    }
+}
