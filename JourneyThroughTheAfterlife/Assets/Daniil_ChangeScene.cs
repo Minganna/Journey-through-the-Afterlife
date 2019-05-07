094faf24750e299ff2f4f318e42320c9c71b073d
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2019-05-07 10:38:15 +0100

    FixedV2

---
+++ JourneyThroughTheAfterlife/Assets/Daniil_ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Daniil_ChangeScene : MonoBehaviour {
+
+	public DialogueTrigger DialogueTrigger;
+
+	void OnTriggerEnter(Collider other)
+	{
+		DialogueTrigger.TriggerDialogue();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player"&&Input.GetKeyDown(KeyCode.E))
+		{
+			SceneManager.LoadScene(3);
+		}
+	}
+}
