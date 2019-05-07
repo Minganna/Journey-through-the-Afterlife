7e06e4026af4fe863119a85b293112107d5fd262
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2019-05-07 03:47:18 +0100

    Boss fight added, brother and exit from the world added. Level almost complete.

---
+++ JourneyThroughTheAfterlife/Assets/Programming/Script/Player/Danill/Daniil_CageActivateFall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Daniil_CageActivateFall : MonoBehaviour {
+
+	public Rigidbody RigBod;
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Minotaur") {
+			RigBod.useGravity = true;
+		}
+	}
+}
